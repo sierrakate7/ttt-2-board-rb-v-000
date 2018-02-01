815212bfbd3abe6cad52e9148f53869a52902c3e
Sierra McCahon <smitche4@mail.umw.edu>
2018-02-01 18:28:27 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,7 @@
 # Define the variable board below.
 board = Array
+<<<<<<< HEAD
 board = [" "," "," "," "," "," "," "," "," "]
+=======
+board = ["Left_top","Middle_top","Right_top","Left_middle","Middle_center","Right_middle","Left_bottom","Middle_botton","Right_bottom"]
+>>>>>>> e6dc839532300b8cf7553bbbc56e5a1fa5542d4d
